4efe6679d2182aa1b93a612db0d7cc23230e3650
Teoslip <tjachiet@gmail.com>
2025-09-03 23:50:46 +0200

    test for each gameboard to be independant

--- src/player.test.js
+++ src/player.test.js
@@ -1,4 +1,5 @@
 import { Player } from "./player.js";
+import { Ship } from "./ship.js";
 
 const player = new Player('human');
 const computer = new Player('computer');
@@ -12,3 +13,17 @@ test('test that the gameboard is made for the two players', () => {
     expect(player.gameboard.getBoard()[0][0].content).toBe('water');
     expect(computer.gameboard.getBoard()[0][0].content).toBe('water');
 });
+
+test('each gameboard is independant', () => {
+    const ship = new Ship(1);
+    
+    player.gameboard.placeShip(ship, 0, 0);
+    computer.gameboard.placeShip(ship, 0, 1);
+
+    expect(player.gameboard.getBoard()[0][0].type.content).toBe('ship');
+    expect(player.gameboard.getBoard()[0][1].type.content).toBe('water');
+
+    expect(computer.gameboard.getBoard()[0][0].type.content).toBe('water');
+    expect(computer.gameboard.getBoard()[0][1].type.content).toBe('ship');
+
+})
